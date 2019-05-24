6b4dd156c34042d0ee5b005a94412f1bf1fb8ef7
shrek1402 <shrek.2000@yandex.ru>
2019-05-24 09:51:49 +0700

    Pre-Beta v0.1

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,22 @@
+#include "pch.h"
+
+constexpr auto _VERSION = "Beta v0.1";
+
 int main(int argc, char** argv)
 {
- 
+	initscr();
+	int col, row;
+	getmaxyx(stdscr, row, col);
+	curs_set(0);
+	refresh();
+	WINDOW *win = newwin(3, col, 0, 0);
+	box(win,0,0);
+	
+	WINDOW *win1 = newwin(row - 3, col, 3, 0); 
+	box(win1, 0, 0);
+	
+	mvaddstr(1, col - 10, VERSION);
+	wrefresh(win1);
+	wrefresh(win);
 	return 0;
 }
