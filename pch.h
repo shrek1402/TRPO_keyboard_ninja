dcccdf9cb7c2bab29f6e1932e1da34b72132133e
shrek1402 <shrek.2000@yandex.ru>
2019-05-24 10:58:19 +0700

    Change 'pch.h'.

--- pch.h
+++ pch.h
@@ -1,13 +1,18 @@
 #ifdef _WIN32
-	#include "pdcurses.h"
+	#include "curses.h"
 #else
     #include <ncurses.h>
 #endif
 
+#include <ctime>
+#include <vector>
+#include <string>
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+bool razmer();
 
 extern int col, row;
