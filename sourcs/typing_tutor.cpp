4eb0b7d8769f2dcadb2956336d1362b0cf91c093
Vadim <Vadcka.casper@yandex.ru>
2019-05-24 12:23:56 +0700

    Correction in the level function

---
+++ sourcs/typing_tutor.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+int level (int row, int col)
+{
+	erase();
+    printRamka(row, col);
+	vector <std::string> mStr = {
+		"  Select the level", 
+		"1. level 1 to 5",
+		"2. level 6 to 10",
+		"3. level 11 to 15",
+        "4. Backward"
+		
+	};
+    	noecho();
+	keypad(stdscr, TRUE);
+	
+	return 0;
+}
