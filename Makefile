dc43865fb9765f23972df20d85442110e87e5f0d
shrek1402 <shrek.2000@yandex.ru>
2019-05-24 15:13:11 +0700

    Temp

---
+++ Makefile
@@ -0,0 +1,47 @@
+CFLAGS = -Wall -Werror -std=c++17 #-Ithird/
+OBJ = g++ $(CFLAGS) -c $< -o $@
+TEST =
+
+.PHONY: clean
+
+all: help
+
+build: folder1 folder2 bin/Keyboard-Ninja.exe copyTXT
+
+folder1:	
+	mkdir -p build
+
+folder2:
+	mkdir -p bin
+	
+bin/Keyboard-Ninja.exe: build/kmenu.o build/Main.o build/typing_tutor.o
+	g++ $(CFLAGS) $^ -lncurses
+	
+build/kmenu.o: sourcs/kmenu.cpp
+	$(OBJ) -lncurses
+	
+build/Main.o: sourcs/Main.cpp
+	$(OBJ) -lncurses
+
+build/typing_tutor.o: sourcs/typing_tutor.cpp
+	$(OBJ) -lncurses
+
+copyDLL: 
+	cp third/pdcurses.dll bin
+	
+copyTXT: 
+	cp sourcs/Welcome.txt bin
+
+run:
+	bin/Keyboard-Ninja.exe Welcome.txt
+
+help:
+	@echo "=========================HELP THERE!========================="
+	@echo ""
+	@echo "1. Build project: Make build"
+	@echo "2. Test: Make test"
+	@echo "3. If you see error 'pdcurses.dll not found': Make copyDLL"
+	@echo ""
+	@echo "=========================    TODO   ========================="
+
+
---
+++ sourcs/kmenu.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+
+constexpr auto VERSION = "Beta v1.0";
+constexpr unsigned int SEC = 1;
+
+
+long long unsigned int printMenu(std::vector <std::string>& _vec)
+{
+	long long unsigned int c = 1;
+	int temp;
+	int col, row;
+	getmaxyx(stdscr, row, col);
+	
+	do{
+		for(long long unsigned int i = 0; i < _vec.size(); i++){
+			move(row/2 + i, col/2 - 9);
+			
+			if (i == c){
+				for (long long unsigned int j=0; j< _vec[i].length(); j++)
+				addch(_vec[i][j] | A_BLINK);
+			}
+			else{
+				printw("%s", _vec[i].c_str());
+			}
+		}
+		
+		if ((temp = getch()) == KEY_UP){
+				if (c != 1)
+					c--;
+				else
+					c = 4;
+		}
+		else if (temp == KEY_DOWN){
+			if (c != 4)
+				c++;
+			else
+				c = 1;
+		}
+		refresh();
+	}while(temp != '\n');
+	return c;
+}
+
+bool razmer()
+{
+	int col, row;
+	getmaxyx(stdscr, row, col);
+	if (row <  10) throw "Uvelich visotu okna";
+	if (col < 40) throw "Uvelich shirinu okna";
+	return true;
+}
+
+int mainMenu(int row, int col)
+{
+    erase();
+    printRamka(row, col);
+	std::vector <std::string> mStr = {
+		"    Menu",
+		"1. Speed mode",
+		"2. typing tutor",
+		"3. Blind seal mode",
+		"4. Exit"
+	};
+
+	noecho();
+	keypad(stdscr, TRUE);
+	
+	return printMenu(mStr);
+}
+int printRamka(int _row, int _col)
+{
+	curs_set(0);
+	refresh();
+	WINDOW *win = newwin(3, _col, 0, 0);
+	box(win,0,0);
+	
+	WINDOW *win1 = newwin(_row - 3, _col, 3, 0); 
+	box(win1, 0, 0);
+	
+	mvaddstr(1, _col - 10, _VERSION);
+	wrefresh(win1);
+	wrefresh(win);
+    return 0;
+}
+
+int slozhnost(int row, int col)
+{
+	erase();
+    printRamka(row, col);
+	vector <std::string> mStr = {
+		"  Slozhn", // TODO eng
+		"1. Eazy",
+		"2. Normal",
+		"3. Hard",
+		"4. Back"
+	};
+
+	noecho();
+	keypad(stdscr, TRUE);
+	
+	return printMenu(mStr);
+}
