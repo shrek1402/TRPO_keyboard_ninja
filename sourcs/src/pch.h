8b29e2e5e97bb3289fb891e763d11001d347979b
shrek1402 <shrek.2000@yandex.ru>
2019-05-28 17:18:57 +0700

    Merge conflict.

--- sourcs/src/pch.h
+++ sourcs/src/pch.h
@@ -2,21 +2,21 @@
 #define PCH_H
 #define PDC_DLL_BUILD
 
-	#ifdef _WIN32
-		#include <curses.h>
-	#else
-		#include <ncurses.h>
-	#endif
+#ifdef _WIN32
+#include <curses.h>
+#else
+#include <ncurses.h>
+#endif
 
-	#include <ctime>
-	#include <vector>
-	#include <string>
-	#include <fstream>
-	#include <iostream>
-	#include <stdlib.h>
-	#include <string>
-	#include <unistd.h>
-	#include <cmath>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+#include <cmath>
 
 	bool razmer();
 	void printWelcomePanel(std::string _str, int row, int col);
