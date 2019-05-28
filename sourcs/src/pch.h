d5ce1d3bd22db0804c66770b7c066ac801b7b190
shrek1402 <shrek.2000@yandex.ru>
2019-05-28 16:13:48 +0700

    Fix color.

--- sourcs/src/pch.h
+++ sourcs/src/pch.h
@@ -19,13 +19,13 @@
 	#include <cmath>
 
 	bool razmer();
-	int printWelcomePanel(std::string _str, int row, int col);
+	void printWelcomePanel(std::string _str, int row, int col);
 	int mainMenu(int row, int col);
 	int printRamka(int _row, int _col);
 	int slozhnost(int row, int col);
 	void speedMode(int slozh, int row, int col);
 	void speedNormal(std::string _dataFile, int row, int col);
-	long long unsigned int printMenu(std::vector <std::string>& _vec);
+	long long unsigned int printMenu(std::vector <std::string>* _vec);
 	void Select_level(int slozh, int row, int col);
 	int level(int row, int col);
 #endif
