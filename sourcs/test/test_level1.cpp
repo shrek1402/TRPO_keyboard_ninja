1fb2b18a7e58b47dc6a4f7a1277c7532f2e5b21f
Vadim <Vadcka.casper@yandex.ru>
2019-05-30 21:39:10 +0700

    fix test

--- sourcs/test/test_level1.cpp
+++ sourcs/test/test_level1.cpp
@@ -3,11 +3,7 @@
 int resultat (int row, int col, int Lessen, unsigned int endTime,
             unsigned int startTime, double sum_proz, int error) {
  
-  int size_x, size_y, xx, yy;
-  size_x = 80;
-  size_y = 20;
-  xx = 20;
-  yy = 15;
+
 
   if (sum_proz < 100) {
 
