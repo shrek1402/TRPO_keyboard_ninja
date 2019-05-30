2e899aeb71c973635c3248e605b5374a63286003
Xaristel <20muxa00@bk.ru>
2019-05-30 17:43:45 +0700

    Added test-function Select_item

--- sourcs/test/num_tests.cpp
+++ sourcs/test/num_tests.cpp
@@ -37,3 +37,42 @@ TEST_CASE("Test [1]", "[SpeedNum]")
     REQUIRE(num_menu(3) == 3);
     REQUIRE(num_menu(5) == 4);
 }
+
+int Select_item(int item)
+{
+    int A[3] = {0, 0, 0};
+    switch (item) {
+    case 1: {
+        SpeedNum(row, col, A);
+        ResultNum(row, col, A, 0);
+        return 1;
+        break;
+    }
+    case 2: {
+        Solving(row, col, A);
+        ResultNum(row, col, A, 1);
+        return 2;
+        break;
+    }
+    case 3: {
+        Calculator(row, col, A);
+        ResultNum(row, col, A, 2);
+        return 3;
+        break;
+    }
+    case 4: {
+        return 4;
+        break;
+    }
+    }
+    return 0;
+}
+
+TEST_CASE("Test [1]", "[SpeedNum]")
+{
+    REQUIRE(Select_item(1) == 1);
+    REQUIRE(Select_item(2) == 2);
+    REQUIRE(Select_item(3) == 3);
+    REQUIRE(Select_item(4) == 4);
+    REQUIRE(Select_item(5) == 0);
+}
