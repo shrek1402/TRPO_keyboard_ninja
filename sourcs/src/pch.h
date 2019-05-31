02858d4d0adbaae4fc2066b3289d6d4dd0df9b3b
Vadim <Vadcka.casper@yandex.ru>
2019-05-31 18:44:58 +0700

    fix bug for 'lessen'

--- sourcs/src/pch.h
+++ sourcs/src/pch.h
@@ -21,7 +21,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 	bool razmer();
 	void printWelcomePanel(std::string _str, int row, int col);
 	int mainMenu(int row, int col);
@@ -38,25 +37,4 @@ using namespace std;
 	int ResultNum (int row, int col, int* A, int flag);
 	int* Solving (int row, int col, int* A);
 	int* Calculator (int row, int col, int* A);
-=======
-bool razmer();
-void printWelcomePanel(std::string _str, int row, int col);
-int mainMenu(int row, int col);
-int printRamka(int _row, int _col);
-int slozhnost(int row, int col);
-void speedMode(int slozh, int row, int col);
-void speedNormal(std::string _dataFile, int row, int col);
-long long unsigned int
-printMenu(std::vector<std::string>* _vec, long long unsigned int punk);
-void Select_level(int slozh, int row, int col);
-int level(int row, int col);
-int Num_Menu(int row, int col);
-void Select_item(int item, int row, int col);
-int* SpeedNum(int row, int col, int* A);
-int ResultNum(int row, int col, int* A, int flag);
-int* Solving(int row, int col, int* A);
-int* Calculator(int row, int col, int* A);
-void Time(int row, int col, int time, int EndTime, int StartTime);
-void TimeLeft(int row, int col);
->>>>>>> 7060a2f73de6bd21ff20031417cf7952799c5e1f
 #endif
